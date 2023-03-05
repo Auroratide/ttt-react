6d48eb70c316a6c3e43470bf449b47e5fb6dd12c
Timothy Foster <tf.auroratide@gmail.com>
2023-03-05 16:11:05 -0600

    Some board state

--- src/Board/Board.stories.tsx
+++ src/Board/Board.stories.tsx
@@ -5,10 +5,37 @@ export default {
 	component: Board,
 } as Meta
 
-export const Default: StoryFn = () => (
-	<div style={{
-		width: '40vw',
-	}}>
-		<Board />
-	</div>
+const makeSquare = (state: 'empty' | 'horizontal' | 'vertical' | 'both') => ({
+	horizontal: state === 'horizontal' || state === 'both',
+	vertical: state === 'vertical' || state === 'both',
+})
+
+export const EmptyBoard: StoryFn = () => (
+	<Container>
+		<Board state={[
+			[makeSquare('empty'), makeSquare('empty'), makeSquare('empty')],
+			[makeSquare('empty'), makeSquare('empty'), makeSquare('empty')],
+			[makeSquare('empty'), makeSquare('empty'), makeSquare('empty')],
+		]} />
+	</Container>
 )
+
+export const SomeFilled: StoryFn = () => (
+	<Container>
+		<Board state={[
+			[makeSquare('empty'), makeSquare('horizontal'), makeSquare('vertical')],
+			[makeSquare('empty'), makeSquare('both'), makeSquare('empty')],
+			[makeSquare('horizontal'), makeSquare('vertical'), makeSquare('horizontal')],
+		]} />
+	</Container>
+)
+
+function Container({ children }: { children: React.ReactNode }) {
+	return (
+		<div style={{
+			width: '40vw',
+		}}>
+			{children}
+		</div>
+	)
+}
--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -2,45 +2,46 @@ import { Square } from '../Square'
 import c from 'classnames'
 import css from './Board.module.css'
 
-export function Board() {
+export interface BoardProps {
+	state: {
+		horizontal: boolean,
+		vertical: boolean,
+	}[][]
+}
+
+export function Board({ state }: BoardProps) {
 	return (
 		<table className={c(css.fullWidth)}>
 			<caption>TTT Board</caption>
 			<tbody>
-				<tr>
-					<td>
-						<Square label="A1" />
-					</td>
-					<td>
-						<Square label="A2" />
-					</td>
-					<td>
-						<Square label="A3" />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<Square label="B1" />
-					</td>
-					<td>
-						<Square label="B2" />
-					</td>
-					<td>
-						<Square label="B3" />
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<Square label="C1" />
-					</td>
-					<td>
-						<Square label="C2" />
-					</td>
-					<td>
-						<Square label="C3" />
-					</td>
-				</tr>
+				{state.map((row, i) => (
+					<Row row={row} index={i} key={i} />
+				))}
 			</tbody>
 		</table>
+	)
+}
+
+function Row({ row, index }: {
+	row: {
+		horizontal: boolean,
+		vertical: boolean,
+	}[],
+	index: number,
+}) {
+	const label = String.fromCharCode(65 + index)
+
+	return (
+		<tr>
+			{row.map((square, squareIndex) => (
+				<td key={`${label}${squareIndex + 1}`}>
+					<Square
+						label={`${label}${squareIndex + 1}`}
+						horizontal={square.horizontal}
+						vertical={square.vertical}
+					/>
+				</td>
+			))}
+		</tr>
 	)
 }
