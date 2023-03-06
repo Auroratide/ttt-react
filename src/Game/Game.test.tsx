162337b4c19239229b0359fa52087daf8fc744d4
Timothy Foster <tf.auroratide@gmail.com>
2023-03-06 06:44:38 -0600

    Refactor test to be more readable

--- src/Game/Game.test.tsx
+++ src/Game/Game.test.tsx
@@ -1,16 +1,22 @@
 import { test, expect } from '@playwright/experimental-ct-react'
 import { Default } from './Game.stories'
 
-const getSquare = (label: string) => `button:has-text("${label}")`
-
 test.describe('Game', () => {
 	test('alternating turns', async ({ mount }) => {
 		const component = await mount(<Default />)
+		const getSquare = (label: string) =>
+			component.getByRole('button', { name: new RegExp(label) })
 	
-		await component.locator(getSquare('B2')).click()
-		await expect(component.locator(getSquare('B2'))).toContainText('vertical')
+		await getSquare('B2').click()
+		await expect(getSquare('B2')).not.toContainText('horizontal')
+		await expect(getSquare('B2')).toContainText('vertical')
 	
-		await component.locator(getSquare('B3')).click()
-		await expect(component.locator(getSquare('B3'))).toContainText('horizontal')
+		await getSquare('B3').click()
+		await expect(getSquare('B3')).toContainText('horizontal')
+		await expect(getSquare('B3')).not.toContainText('vertical')
+
+		await getSquare('B3').click()
+		await expect(getSquare('B3')).toContainText('horizontal')
+		await expect(getSquare('B3')).toContainText('vertical')
 	})
 })
